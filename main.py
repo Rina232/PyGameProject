76c4977f12affeb23f1814432ff994e7539367f7
Rin232 <ekaterina>
2022-12-25 03:28:32 +0300

    Начало кода

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import pygame
 import os
 import sys
 
+pygame.init()
+
+FPS = 50
+WIDTH = 500
+HEIGHT = 600
+STEP = 50
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+player = None
 all_sprites = pygame.sprite.Group()
 
 
@@ -21,52 +32,75 @@ def load_image(name, colorkey=None):
     return image
 
 
-class AnimatedSprite(pygame.sprite.Sprite):
-    def __init__(self, sheet, columns, rows, x, y):
-        super().__init__(all_sprites)
-        self.frames = []
-        self.cut_sheet(sheet, columns, rows)
-        self.cur_frame = 0
-        self.image = self.frames[self.cur_frame]
-        self.rect = self.rect.move(x, y)
-        self.k = 0
-
-    def cut_sheet(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-                                sheet.get_height() // rows)
-        for j in range(rows):
-            for i in range(columns):
-                frame_location = (self.rect.w * i, self.rect.h * j)
-                self.frames.append(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)))
-
-    def update(self):
-        self.k += 1
-        if self.k % 5 == 0:
-            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-            self.image = self.frames[self.cur_frame]
+def terminate():
+    pygame.quit()
+    sys.exit()
 
 
-def main():
-    pygame.init()
-    size = width, height = 400, 300
-    screen = pygame.display.set_mode(size)
-    clock = pygame.time.Clock()
+def start_screen():  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  ""]
 
-    spaceship = AnimatedSprite(load_image("spaceship.png"), 3, 4, 100, 100)
+    fon = pygame.transform.scale(load_image(f'Picture\\SpaceBackground1.png'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    text_coord = 100
+    for line in intro_text:
+        font = pygame.font.SysFont('Arial', 30)
+        string_rendered = font.render(line, 1, pygame.Color(211, 202, 219))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
 
-    running = True
-    while running:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-        screen.fill(pygame.Color("black"))
-        all_sprites.draw(screen)
-        all_sprites.update()
+                terminate()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return
         pygame.display.flip()
-        clock.tick(50)
-    pygame.quit()
+        clock.tick(FPS)
 
 
-if __name__ == '__main__':
-    main()
+# class AnimatedSprite(pygame.sprite.Sprite):
+#    def __init__(self, sheet, columns, rows, x, y):
+#        super().__init__(all_sprites)
+#        self.frames = []
+#        self.cut_sheet(sheet, columns, rows)
+#        self.cur_frame = 0
+#        self.image = self.frames[self.cur_frame]
+#        self.rect = self.rect.move(x, y)
+#        self.k = 0
+#
+#    def cut_sheet(self, sheet, columns, rows):
+#        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+#                                sheet.get_height() // rows)
+#        for j in range(rows):
+#            for i in range(columns):
+#                frame_location = (self.rect.w * i, self.rect.h * j)
+#                self.frames.append(sheet.subsurface(pygame.Rect(
+#                    frame_location, self.rect.size)))
+#
+#    def update(self):
+#        self.k += 1
+#        if self.k % 5 == 0:
+#            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+#            self.image = self.frames[self.cur_frame]
+
+start_screen()
+
+#    spaceship = AnimatedSprite(load_image("Picture\shot2.png"), 1, 5, 63, 32)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(pygame.Color(0, 0, 0))
+    pygame.display.flip()
+    clock.tick(FPS)
+
+terminate()
